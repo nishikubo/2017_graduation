7e5c4bea4ca7375dc084f40b04e87420141e1886
Kosugi <twilight.snowtopaz@gmail.com>
2017-11-10 13:58:54 +0900

    no message

---
+++ Project55/Assets/Kosugi/Script/CameraManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
