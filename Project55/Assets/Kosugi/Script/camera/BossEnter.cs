4a0dd4cb5a24a22af969e6678165d29f3896182c
Kosugi <twilight.snowtopaz@gmail.com>
2017-11-17 16:15:39 +0900

    主にカメラをいじった

---
+++ Project55/Assets/Kosugi/Script/camera/BossEnter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossEnter : MonoBehaviour
+{
+	void Start ()
+	{
+		
+	}
+	
+	void Update ()
+	{
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Player")
+        {
+            GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraController>().mBoss
+                = !GameObject.FindGameObjectWithTag("MainCamera").GetComponent<CameraController>().mBoss;
+            Destroy(gameObject);
+            print("enter");
+        }
+    }
+}
--- Project55/Assets/Kosugi/Script/camera/CameraController.cs
+++ Project55/Assets/Kosugi/Script/camera/CameraController.cs
@@ -21,15 +21,21 @@ public class CameraController : MonoBehaviour
     [Header("カメラの表示領域")]
     private Vector3 mCameraTopL, mCameraTopR, mCameraBottomL, mCameraBottomR;
 
-    [Header("カメラの横幅")]
+    [SerializeField, Header("カメラの横幅")]
     private float mCameraWidth;
-    [Header("カメラの縦幅")]
+    [SerializeField, Header("カメラの縦幅")]
     private float mCameraHeight;
 
     [Header("カメラの座標")]
     private Vector3 mNewPosition;
     [Header("移動制限座標")]
     private Vector3 mLimitPosition;
+
+    [Header("ボス突入フラグ")]
+    public bool mBoss;
+
+    [Header("Stateの管理")]
+    private StateManager mStateManager;
 
     /// <summary>
     /// カメラの表示領域を緑ラインで表示
@@ -45,6 +51,9 @@ public class CameraController : MonoBehaviour
 
     void Start()
     {
+        //取得
+        mStateManager = GameObject.FindGameObjectWithTag("Manager").GetComponent<StateManager>();
+
         //プレイヤーキャラを取得
         mPlayer = GameObject.FindGameObjectWithTag("Player");
         mOffset = transform.position - mPlayer.transform.position;
@@ -52,7 +61,16 @@ public class CameraController : MonoBehaviour
         mStageRect = GameObject.Find("Floor").GetComponent<StageRect>();
     }
 
+    
+
     void LateUpdate()
+    {
+        if (!mBoss)
+            NormaCamera();
+        else
+            BossCamera();
+    }
+    void NormaCamera()
     {
         Rect mRect = mStageRect.GetStageRect();
         float mNewX = 0f;
@@ -62,16 +80,21 @@ public class CameraController : MonoBehaviour
         mNewPosition = mPlayer.transform.position + mOffset + Vector3.up * 3f;
 
         CameraSetting();
-        
+
         //カメラの稼働領域をステージ領域に制限
         mNewX = Mathf.Clamp(mNewPosition.x, mRect.xMin + mCameraWidth / 2, mRect.xMax - mCameraWidth / 2);
         mNewY = Mathf.Clamp(mNewPosition.y, 0, mRect.yMax - mCameraHeight / 2);
-        
+
         //座標をカメラ位置に設定
-        mLimitPosition = new Vector3(mNewX, mNewY, this.transform.position.z);
+        mLimitPosition = new Vector3(mNewX, mNewY, transform.position.z);
         transform.position = Vector3.Lerp(transform.position, mLimitPosition, 5.0f * Time.deltaTime);
     }
-
+    void BossCamera()
+    {
+        Vector3 mBossPos= GameObject.Find("BossCameraPos").transform.position;
+        mLimitPosition = new Vector3(mBossPos.x, mBossPos.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, mLimitPosition, 3.0f * Time.deltaTime);
+    }
     /// <summary>
     /// ビューポート座標をワールド座標に変換
     /// </summary>
